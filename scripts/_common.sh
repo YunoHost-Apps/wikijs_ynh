e153494b48d4d7f33b9d85ddd5c015e030029c16
ericgaspar <junk.eg@free.fr>
2020-09-22 15:45:01 +0200

    Upgrade to v.2.5.144

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="postgresql postgresql-contrib"
 
-NODEJS_VERSION=10
+NODEJS_VERSION=12
 
 #=================================================
 # PERSONAL HELPERS
